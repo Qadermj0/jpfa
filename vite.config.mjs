8c1b8ecf0ac54e0cb9e586caca34e4421986dba5
Qadermj0 <qadermajali56@gmail.com>
2025-07-15 18:43:11 +0300

    Add GitHub Actions workflow for deployment

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+   base: '/jpfa/',
   plugins: [react()],
   server: {
     port: 5175, // يتطابق مع المنفذ الافتراضي لـ Vite 7.0.0
